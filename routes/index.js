2197412c6209117451788a6a3c44e88688f98dfb
Nishil <nishilkapadia17@gmail.com>
2020-07-25 13:07:55 +0530

    :bug: api refactor

--- routes/index.js
+++ routes/index.js
@@ -1,34 +1,11 @@
 const express = require('express');
 const router = express.Router();
 const User = require('../models/User');
-const {ensureAuth} = require('../middleware/auth')
+const { ensureAuth } = require('../middleware/auth');
 
 // GET to /
 router.get('/', (req, res, next) => {
-	res.render('index')
+	res.render('index');
 });
-
-const sampleObj = {
-	googleId: '141311414',
-	displayName: 'Alex M.',
-	firstName: 'Alex',
-	role: 'student',
-	email: 'demo@gmail.com',
-	image: 'https://image.com',
-};
-
-
-// POST: '/demo' - > create route will add a sample object without login ( test )
-router.get('/demo', async (req, res, next) => {
-	try {
-		const user = await User.create(sampleObj);
-
-		res.json({ user });
-	} catch (err) {
-		console.log(err);
-		next(err);
-	}
-});
-
 
 module.exports = router;
