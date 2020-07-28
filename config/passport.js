0c3f729f83d3f141d78c5a97f447b9ab9807e491
piyush2003m <piyush2003m@gmail.com>
2020-07-28 12:46:16 +0530

    authChange

--- config/passport.js
+++ config/passport.js
@@ -9,7 +9,7 @@ module.exports = function (passport) {
 			{
 				clientID: process.env.GOOGLE_CLIENT_ID,
 				clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-				callbackURL: 'http://localhost:5000/auth/google/callback',
+				callbackURL: '/auth/google/callback',
 			},
 			async (accessToken, refreshToken, profile, done) => {
 				try {
